92a01a3fc8ce54875303b5280c01ea368c4a3925
Hank Magan <bryant.magan@gmail.com>
2021-01-18 23:49:32 -0500

    Initial commit; created homepage

---
+++ js/index.js
@@ -0,0 +1,46 @@
+$(document).ready(function(){
+  setTimeout(function(){
+    let i = 0.0;
+    $("#about-us").css("pointer-events", "auto");
+    setInterval(function(){
+      i += 0.01;
+      $("#about-us").css("opacity", i);
+    }, 10);
+  }, 3500);
+});
+
+(function () {
+
+    var bv = new Bideo();
+    bv.init({
+      // Video element
+      videoEl: document.querySelector('#background-video'),
+  
+      // Container element
+      container: document.querySelector('body'),
+  
+      // Resize
+      resize: true,
+  
+      // autoplay: false,
+  
+      isMobile: window.matchMedia('(max-width: 768px)').matches,
+  
+      playButton: document.querySelector('#play'),
+      pauseButton: document.querySelector('#pause'),
+  
+      // Array of objects containing the src and type
+      // of different video formats to add
+      src: [
+        {
+          src: 'img/code-background.mp4',
+          type: 'video/mp4'
+        }
+      ],
+  
+      // What to do once video loads (initial frame)
+      onLoad: function () {
+        document.querySelector('#video-cover').style.display = 'none';
+      }
+    });
+  }());
