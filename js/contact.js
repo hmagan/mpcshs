adc5d29e38fb880294775d262e6f6568a41145c6
hmagan <bryant.magan@gmail.com>
2021-02-01 01:07:56 -0500

    started on contact.html

--- js/contact.js
+++ js/contact.js
@@ -1,3 +1,26 @@
+function copyOnClick(){
+    //use getElementsByClassName bc ClipboardJS requires an HTMLCollection
+    let elem = document.getElementsByClassName("copy-on-click");
+    let text = $(this);
+    let idx = -1;
+    for(let i = 0; i < elem.length; i++){
+        for(let j = 0; j < text.length; j++){
+            if(elem[i].innerHTML === text[j].innerHTML)
+                idx = i;
+                break;
+        }
+    }
+    console.log(idx);
+    let clipboard = new ClipboardJS(elem, {
+        text: function(){
+            return elem[idx].innerHTML;
+        }
+    });
+    console.log("<=======================================>");
+}
+
 $(document).ready(function(){
     $("#contact").addClass("nav-button-highlight");
+    //have to click twice for some reason fix later
+    $(".copy-on-click").click(copyOnClick);
 });
