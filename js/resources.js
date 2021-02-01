967178a840fd7d53c94c8f86cbda347e641620a9
hmagan <bryant.magan@gmail.com>
2021-02-01 01:07:22 -0500

    resources.html mostly finalized

--- js/resources.js
+++ js/resources.js
@@ -1,3 +1,20 @@
+function getReboundScrollPosition(){
+    return innerHeight / 2;
+}
+
 $(document).ready(function(){
     $("#resources").addClass("nav-button-highlight");
+    $(".jump-to-mooc").click(function(){
+        //wait 1ms to allow browser to jump down before rebounding
+        setTimeout(function(){
+            let pos = $(document).scrollTop();
+            $(document).scrollTop(pos - getReboundScrollPosition());
+            $("#mooc-wrapper").css("background-color", "#ebe859"); //yellow
+            setTimeout(function(){
+                $("#mooc-wrapper").css("transition", "1.5s");
+                $("#mooc-wrapper").css("background-color", "#3A3A3A"); //var(--black)
+            }, 250);
+            $("#mooc-wrapper").css("transition", "0s");
+        }, 1);
+    });
 });
