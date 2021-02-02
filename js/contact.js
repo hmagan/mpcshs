a0ce8403a3c39325ddddf510b28df4d5b8ecfbea
Hank Magan <bryant.magan@gmail.com>
2021-02-02 01:13:14 -0500

    fixed copy on click function

--- js/contact.js
+++ js/contact.js
@@ -1,26 +1,22 @@
-function copyOnClick(){
-    //use getElementsByClassName bc ClipboardJS requires an HTMLCollection
-    let elem = document.getElementsByClassName("copy-on-click");
-    let text = $(this);
-    let idx = -1;
-    for(let i = 0; i < elem.length; i++){
-        for(let j = 0; j < text.length; j++){
-            if(elem[i].innerHTML === text[j].innerHTML)
-                idx = i;
-                break;
-        }
-    }
-    console.log(idx);
-    let clipboard = new ClipboardJS(elem, {
+function copyOnClick(selector){
+    //make new clipboard object
+    let clipboard = new ClipboardJS(selector, {
         text: function(){
-            return elem[idx].innerHTML;
+            //copy string directly to clipboard
+            return selector.innerHTML;
         }
     });
-    console.log("<=======================================>");
 }
 
 $(document).ready(function(){
+    //add indicator on navbar
     $("#contact").addClass("nav-button-highlight");
-    //have to click twice for some reason fix later
-    $(".copy-on-click").click(copyOnClick);
+    $(".copy-on-click").click(function(){
+        copyOnClick(this);
+    });
+    //must trigger click or else it requires two clicks to copy (idk either but it works??)
+    $(".copy-on-click").trigger("click");
+    $(".copy-on-click").click(function(){
+        $(this).css("background-color", "var(--mp-green)");
+    });
 });
