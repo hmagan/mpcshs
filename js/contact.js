5e0ee10ff70507492c57e7a9ff44faa504c59d59
hmagan <bryant.magan@gmail.com>
2021-02-10 22:04:13 -0500

    fleshed out copy on click and added more contacts

--- js/contact.js
+++ js/contact.js
@@ -1,9 +1,8 @@
 function copyOnClick(selector){
-    //make new clipboard object
     let clipboard = new ClipboardJS(selector, {
         text: function(){
-            //copy string directly to clipboard
-            return selector.innerHTML;
+            //copy string directly to clipboard without "Copied!" appended to the end
+            return selector.textContent.substring(0, selector.textContent.length - 7);
         }
     });
 }
@@ -17,6 +16,27 @@ $(document).ready(function(){
     //must trigger click or else it requires two clicks to copy (idk either but it works??)
     $(".copy-on-click").trigger("click");
     $(".copy-on-click").click(function(){
-        $(this).css("background-color", "var(--mp-green)");
+        let elem = $(this)[0];
+        elem.parentNode.getElementsByClassName("copy-text")[0].textContent = "Copied!";
+        elem.style.borderColor = "yellow";
+        setTimeout(function(){
+            elem.style.borderColor = "var(--mp-green)";
+            elem.parentNode.getElementsByClassName("copy-text")[0].textContent = "Copy";
+        }, 1000);
+    });
+    let fadeIn, popup, opacity;
+    $(".copy-on-click").mouseenter(function(){
+        popup = $(this)[0].parentNode.getElementsByClassName("copy-text")[0];
+        opacity = 0.0;
+        fadeIn = setInterval(function(){
+            if(opacity < 1){
+                opacity += 0.01
+                popup.style.opacity = opacity;
+            }
+        }, 4);
+    }).mouseout(function(){
+        popup.style.opacity = 0.0;
+        popup.textContent = "Copy";
+        clearInterval(fadeIn);
     });
 });
